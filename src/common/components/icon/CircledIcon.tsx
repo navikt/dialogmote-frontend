0c5b357e1753cb0d263fcc2cdd3cd18be669896b
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2022-01-21 15:02:58 +0100

    Add possibility to use icons from design system in DialogmotePanel (#6)

---
+++ src/common/components/icon/CircledIcon.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+const Circle = styled.span`
+  height: 3rem;
+  width: 3rem;
+  background-color: #BCF3FF;
+  border-radius: 50%;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+interface Props {
+  icon: ReactNode;
+}
+
+const CircledIcon = ({ icon }: Props) => {
+  return <Circle>{icon}</Circle>;
+};
+
+export default CircledIcon;
