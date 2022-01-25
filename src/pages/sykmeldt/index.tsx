7d1ff78e8f751807cac99f257f4c15018fbe7413
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-01-25 14:01:40 +0100

    Add ReferaterPanel to home page for sykmeldt and arbeidsgiver

--- src/pages/sykmeldt/index.tsx
+++ src/pages/sykmeldt/index.tsx
@@ -1,26 +1,40 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../../styles/Home.module.css";
-import { useBrevSM } from "@/common/api/queries/sykmeldt/brevQueriesSM";
-import { useMotebehovSM } from "@/common/api/queries/sykmeldt/motebehovQueriesSM";
+import {useBrevSM} from "@/common/api/queries/sykmeldt/brevQueriesSM";
+import {useMotebehovSM} from "@/common/api/queries/sykmeldt/motebehovQueriesSM";
+import ReferaterPanel from "@/common/components/referat/ReferaterPanel";
 import VideoPanel from "@/common/components/video/VideoPanel";
+import type {NextPage} from "next";
+import Head from "next/head";
+import React from "react";
+import styles from "../../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const brev = useBrevSM();
-  const motebehov = useMotebehovSM();
+    const brev = useBrevSM();
+    const motebehov = useMotebehovSM();
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Dialogmøte SM</title>
-      </Head>
+    if (brev.isError) {
+        return <div>Her ble det noe feil</div>;
+    }
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>SM!</h1>
-        <VideoPanel />
-      </main>
-    </div>
-  );
+    if (brev.isSuccess) {
+        const referater = brev.data.filter(
+            (hendelse) => hendelse.brevType === "REFERAT"
+        );
+
+        return (
+            <div className={styles.container}>
+                <Head>
+                    <title>Dialogmøte SM</title>
+                </Head>
+
+                <main className={styles.main}>
+                    <ReferaterPanel referater={referater}/>
+                    <VideoPanel/>
+                </main>
+            </div>
+        );
+    }
+
+    return <div>Vis en loading-animasjon her..</div>;
 };
 
 export default Home;
