fd49a7532d834f65d972e51be0fa34639e4b990f
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-02-08 15:57:40 +0100

    Test to see if it works in preprod

--- src/common/publicEnv.ts
+++ src/common/publicEnv.ts
@@ -3,7 +3,7 @@ const isMockBackend: boolean = process.env["MOCK_BACKEND"] === "true";
 const basePath: string = process.env["BASE_PATH"] as string;
 const environment: string = process.env["ENVIRONMENT"] as string;
 const decoratorEnv = process.env["DECORATOR_ENV"] === "prod" ? "prod" : "dev";
-const dittSykefravaerUrl: string = process.env["NEXT_PUBLIC_DITT_SYKEFRAVAER_ROOT"] as string;
-const dineSykmeldteUrl: string = process.env["NEXT_PUBLIC_DINE_SYKMELDTE_ROOT"] as string;
+const dittSykefravaerUrl: string = process.env.NEXT_PUBLIC_DITT_SYKEFRAVAER_ROOT as string;
+const dineSykmeldteUrl: string = process.env.NEXT_PUBLIC_DINE_SYKMELDTE_ROOT as string;
 
 export { isOpplaering, isMockBackend, basePath, environment, decoratorEnv, dittSykefravaerUrl, dineSykmeldteUrl };
