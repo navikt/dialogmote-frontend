526972d5710ec29b7777a779b883f31e4754b01d
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-02-17 14:39:54 +0100

    Add sykmeldt name to DialogmoteData respons

--- src/server/data/types/internal/DialogmoteType.ts
+++ src/server/data/types/internal/DialogmoteType.ts
@@ -4,6 +4,7 @@ import { DialogMotebehov } from "@/server/data/types/internal/DialogMotebehovTyp
 
 export interface DialogmoteData {
   isSykmeldt: boolean;
+  sykmeldtName?: string;
   moteinnkalling?: Brev;
   referater: Referat[];
   motebehov?: DialogMotebehov;
