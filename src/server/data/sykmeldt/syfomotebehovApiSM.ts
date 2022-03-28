a269849af736554e60d7cc785e163f4623fa86fc
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-03-28 10:11:15 +0200

    Add new api version

--- src/server/data/sykmeldt/syfomotebehovApiSM.ts
+++ src/server/data/sykmeldt/syfomotebehovApiSM.ts
@@ -17,7 +17,7 @@ export const postMotebehovSM = async (
     console.log("tokenSet", req.tokenSet);
     const svar: ExtMotebehovSvar = req.body;
     await post(
-      `${serverEnv.SYFOMOTEBEHOV_HOST}/syfomotebehov/api/v2/arbeidstaker/motebehov`,
+      `${serverEnv.SYFOMOTEBEHOV_HOST}/syfomotebehov/api/v3/arbeidstaker/motebehov`,
       svar,
       {
         accessToken: req.tokenSet.access_token,
