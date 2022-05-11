29857893883f9cafe53e4dfd90cd653e1dc22cd9
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-05-11 09:30:36 +0200

    Add SM-path in AG breadcrumbs (#107)

--- src/common/breadcrumbs/BreadcrumbsAppenderAG.tsx
+++ src/common/breadcrumbs/BreadcrumbsAppenderAG.tsx
@@ -2,17 +2,22 @@ import { ReactElement, useEffect } from "react";
 import { useRouter } from "next/router";
 import { setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 import { createBreadcrumbsAG } from "@/common/breadcrumbs/index";
+import { useDialogmoteDataAG } from "@/common/api/queries/arbeidsgiver/dialogmoteDataQueryAG";
 import { useNarmesteLederId } from "@/common/hooks/useNarmesteLederId";
 
 export const BreadcrumbsAppenderAG = (): ReactElement => {
   const { pathname } = useRouter();
   const narmestelederId = useNarmesteLederId();
+  const dialogmoteData = useDialogmoteDataAG();
+  const sykmeldtName = dialogmoteData.data?.sykmeldt?.navn;
 
   useEffect(() => {
-    if (narmestelederId) {
-      setBreadcrumbs(createBreadcrumbsAG(pathname, narmestelederId));
+    if (narmestelederId && sykmeldtName) {
+      setBreadcrumbs(
+        createBreadcrumbsAG(pathname, sykmeldtName, narmestelederId)
+      );
     }
-  }, [narmestelederId, pathname]);
+  }, [sykmeldtName, narmestelederId, pathname]);
 
   return <></>;
 };
