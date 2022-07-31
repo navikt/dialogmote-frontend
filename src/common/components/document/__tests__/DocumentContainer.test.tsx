c0d422837cf312d43d0daaa135206a1cc1304f51
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-08-01 00:41:14 +0200

    add example tests

---
+++ src/common/components/document/__tests__/DocumentContainer.test.tsx
@@ -0,0 +1,93 @@
+import { useMutateBrevLest } from "@/common/api/queries/brevQueries";
+import { createDocumentComponent } from "@/fixtures/brev";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import DocumentContainer from "../DocumentContainer";
+
+describe("DocumentContainer", () => {
+  const mutateSpy = jest.fn();
+
+  beforeEach(() => {
+    (useMutateBrevLest as jest.Mock).mockReturnValue({
+      mutate: mutateSpy,
+      isLoading: false,
+    });
+  });
+
+  afterEach(() => {
+    mutateSpy.mockReset();
+  });
+
+  test("should render", () => {
+    const document = [
+      createDocumentComponent({ texts: ["test-text-1"] }),
+      createDocumentComponent(),
+    ];
+
+    render(
+      <DocumentContainer
+        document={document}
+        brevUuid="brev_uuid"
+        title="Test_title"
+      >
+        <div>Test div</div>
+      </DocumentContainer>
+    );
+
+    expect(screen.getByText("test-text-1")).toBeInTheDocument();
+  });
+
+  test("should render with legacy header", () => {
+    const document = [
+      createDocumentComponent({ type: "PARAGRAPH" }),
+      createDocumentComponent(),
+    ];
+
+    render(
+      <DocumentContainer
+        document={document}
+        brevUuid="brev_uuid"
+        title="Test_title"
+      >
+        <div>Test div</div>
+      </DocumentContainer>
+    );
+
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "Test_title"
+    );
+  });
+
+  test("should invoke mutation.mutate when lestDato is undefined", () => {
+    const document = [createDocumentComponent(), createDocumentComponent()];
+
+    render(
+      <DocumentContainer
+        document={document}
+        brevUuid="brev_uuid"
+        title="Test_title"
+      >
+        <div>Test div</div>
+      </DocumentContainer>
+    );
+
+    expect(mutateSpy).toHaveBeenCalledWith("brev_uuid");
+  });
+
+  test("should not invoke mutation.mutate when lestDato is defined", () => {
+    const document = [createDocumentComponent(), createDocumentComponent()];
+
+    render(
+      <DocumentContainer
+        document={document}
+        brevUuid="brev_uuid"
+        lestDato="date"
+        title="Test_title"
+      >
+        <div>Test div</div>
+      </DocumentContainer>
+    );
+
+    expect(mutateSpy).not.toHaveBeenCalled();
+  });
+});
---
+++ src/fixtures/brev.ts
@@ -0,0 +1,40 @@
+import { DocumentComponent } from "types/client/brev";
+import { Brev } from "types/shared/brev";
+
+export const createDocumentComponent = (
+  props?: Partial<DocumentComponent>
+): DocumentComponent => {
+  return {
+    type: "PARAGRAPH",
+    title: "TEST_HEADER",
+    texts: ["Test_text_1", "Test_text_2"],
+    key: "ARBEIDSRETTET_REHABILITERING",
+    ...props,
+  };
+};
+
+export const createInnkallelseBrev = (props?: Partial<Brev>): Brev => {
+  return {
+    uuid: "brev_uuid",
+    deltakerUuid: "deltaker_uuid",
+    createdAt: "2019-11-08",
+    brevType: "INNKALT",
+    digitalt: true,
+    fritekst: "Her kommer det en fritekst",
+    sted: "sted-felt",
+    tid: "2020-11-08",
+    videoLink: "videolenke-felt",
+    document: [createDocumentComponent(), createDocumentComponent()],
+    virksomhetsnummer: "virksomhetsnummer-felt",
+    lestDato: null,
+    svar: null,
+    ...props,
+  };
+};
+
+export const createReferatBrev = (props?: Partial<Brev>): Brev => {
+  return {
+    ...createInnkallelseBrev({ brevType: "INNKALT" }),
+    ...props,
+  };
+};
