661978e375368d2fdbd24fce3616d3e3fdfc1134
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-11-28 01:07:56 +0100

    Add focus to error summary

--- src/common/components/motebehov/MotebehovErrorSummary.tsx
+++ src/common/components/motebehov/MotebehovErrorSummary.tsx
@@ -1,5 +1,5 @@
 import { ErrorSummary } from "@navikt/ds-react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const texts = {
   feiloppsummeringTittel: "For å gå videre må du rette opp følgende:",
@@ -15,9 +15,15 @@ interface Props {
 }
 
 export const MotebehovErrorSummary = ({ errors }: Props) => {
+  const errorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (errors.length > 0) errorRef?.current?.focus();
+  }, [errors]);
+
   if (errors.length > 0) {
     return (
-      <ErrorSummary heading={texts.feiloppsummeringTittel}>
+      <ErrorSummary heading={texts.feiloppsummeringTittel} ref={errorRef}>
         {errors.map((err, index) => {
           return (
             <ErrorSummary.Item key={index} href={err.href}>
