340cd24036c04ff3a391013021b02d94a5ca47e2
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-12-01 11:43:23 +0100

    Correct link to video meeting instructions

--- src/common/constants/staticUrls.ts
+++ src/common/constants/staticUrls.ts
@@ -5,5 +5,5 @@ export const KONTAKT_INFO_URL_SM =
 export const DIALOGMOTE_INFO_URL =
   "https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/relatert-informasjon/slik-folger-du-opp-sykmeldte/dialogmote-2-og-3-nav_kap";
 export const VIDEOMOTE_INFO_URL =
-  "https://www.nav.no/no/nav-og-samfunn/kontakt-nav/slik-deltar-du-i-videomote-med-nav";
+  "https://www.nav.no/brukerstotte#videomote-med-nav";
 export const TIDSLINJE_URL = "/tidslinjen";
