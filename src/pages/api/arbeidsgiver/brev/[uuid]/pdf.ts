fab8bd152d71c8a74a8f6a7b2386eb11f4f775ca
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-01-26 00:04:42 +0100

    Add ts lint plugin (#159)
    * Add ts-lint
    
    * Fix linter warnings

--- src/pages/api/arbeidsgiver/brev/[uuid]/pdf.ts
+++ src/pages/api/arbeidsgiver/brev/[uuid]/pdf.ts
@@ -5,10 +5,10 @@ import { withSentry } from "@sentry/nextjs";
 import { fetchBrevPdfAG } from "@/server/data/arbeidsgiver/isDialogmoteApiAG";
 import getIdportenToken from "@/server/auth/idporten/idportenToken";
 
-const handler = nc<NextApiRequest, NextApiResponse<any>>(ncOptions)
+const handler = nc<NextApiRequest, NextApiResponse>(ncOptions)
   .use(getIdportenToken)
   .use(fetchBrevPdfAG)
-  .get(async (req: NextApiRequest, res: NextApiResponse & { pdf: any }) => {
+  .get(async (req: NextApiRequest, res: NextApiResponse & { pdf: unknown }) => {
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", 'inline; filename="brev.pdf"');
     res.end(res.pdf);
