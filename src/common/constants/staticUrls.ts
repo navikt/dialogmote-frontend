6ab8077cc7e0b2a3fc2befe1736b50e80a5f6c0d
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-02-22 11:59:41 +0100

    Update link to om Dialogmøter (#164)

--- src/common/constants/staticUrls.ts
+++ src/common/constants/staticUrls.ts
@@ -3,7 +3,7 @@ export const KONTAKT_INFO_URL_AG = "https://arbeidsgiver.nav.no/kontakt-oss/";
 export const KONTAKT_INFO_URL_SM =
   "https://www.nav.no/person/kontakt-oss/nb/skriv-til-oss";
 export const DIALOGMOTE_INFO_URL =
-  "https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/relatert-informasjon/slik-folger-du-opp-sykmeldte/dialogmote-2-og-3-nav_kap";
+  "https://www.nav.no/arbeidsgiver/oppfolging-sykmeldte#dialogmote-2og3";
 export const VIDEOMOTE_INFO_URL =
   "https://www.nav.no/brukerstotte#videomote-med-nav";
 export const TIDSLINJE_URL = "/tidslinjen";
