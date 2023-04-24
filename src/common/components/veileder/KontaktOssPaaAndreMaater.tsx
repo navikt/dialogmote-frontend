a56ddba565c72c639128ead09ffbc4bd1f4246d1
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-04-24 14:23:28 +0200

    Update link to Skriv til oss

--- src/common/components/veileder/KontaktOssPaaAndreMaater.tsx
+++ src/common/components/veileder/KontaktOssPaaAndreMaater.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { BodyLong } from "@navikt/ds-react";
-import { KontaktOssLink } from "@/common/components/kontaktoss/KontaktOssLink";
+import { BodyLong, Link } from "@navikt/ds-react";
 import VeilederGuidePanel from "@/common/components/veileder/VeilederGuidePanel";
+import { useAmplitude } from "@/common/hooks/useAmplitude";
+import { KONTAKT_INFO_SYK_URL_SM } from "@/common/constants/staticUrls";
+import { Events } from "@/common/amplitude/events";
 
 export const KontaktOssPaaAndreMaater = () => {
+  const { trackEvent } = useAmplitude();
   return (
     <VeilederGuidePanel>
       <BodyLong spacing>
         Har du spørsmål om sykepenger, arbeidsavklaringspenger eller noe annet
         enn det vi skal snakke om i et dialogmøte, kan du&nbsp;
-        <KontaktOssLink linkText={" kontakte oss på andre måter."} />
+        <Link
+          href={KONTAKT_INFO_SYK_URL_SM}
+          onClick={() => trackEvent(Events.KontaktOss)}
+        >
+          kontakte oss på andre måter.
+        </Link>
       </BodyLong>
     </VeilederGuidePanel>
   );
--- src/common/constants/staticUrls.ts
+++ src/common/constants/staticUrls.ts
@@ -2,6 +2,8 @@ export const PERSONVERN_URL = "https://www.nav.no/personvern";
 export const KONTAKT_INFO_URL_AG = "https://arbeidsgiver.nav.no/kontakt-oss/";
 export const KONTAKT_INFO_URL_SM =
   "https://www.nav.no/person/kontakt-oss/nb/skriv-til-oss";
+export const KONTAKT_INFO_SYK_URL_SM =
+  "https://innboks.nav.no/s/skriv-til-oss?category=Helse";
 export const DIALOGMOTE_INFO_URL =
   "https://www.nav.no/arbeidsgiver/oppfolging-sykmeldte#dialogmote-2og3";
 export const VIDEOMOTE_INFO_URL =
