7a28b36f69c9f4a9d76a711b6dba3c784f9fea09
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-04-27 14:02:12 +0200

    Add rtl/react-hooks and add test

---
+++ src/common/hooks/useErrorSummaryFormatter.test.ts
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useErrorSummaryFormatter } from "./useErrorSummaryFormatter";
+import { FieldErrors } from "react-hook-form";
+
+describe("useErrorSummaryFormatter", () => {
+  it("should format FieldErrors to ErrorValues", () => {
+    const fieldErrors: FieldErrors = {
+      field1: {
+        type: "required",
+        message: "error at field 1",
+      },
+      field2: undefined,
+    };
+    const { result } = renderHook(() => useErrorSummaryFormatter(fieldErrors));
+    const formatted = result.current;
+
+    expect(formatted).toStrictEqual([
+      { href: "#field1", text: "error at field 1" },
+    ]);
+  });
+});
