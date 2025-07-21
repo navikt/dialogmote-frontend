fc23dfc491946956bb452a608ae9e1aa101af2b8
Håkon Møller <hakon.salomonsen.moller@nav.no>
2025-07-21 12:34:23 +0200

    Add UX signals panel to sykmeldt meld page (#1423)

---
+++ src/common/components/ux-signals-panel/ux-signals-panel.tsx
@@ -0,0 +1,14 @@
+import useUxSignals from "@/common/hooks/useUxSignals";
+
+const UxSignalsPanel = () => {
+  useUxSignals(true);
+
+  return (
+    <div
+      data-uxsignals-embed="panel-hs78z2ahch"
+      style={{ marginBottom: "2rem" }}
+    />
+  );
+};
+
+export default UxSignalsPanel;
