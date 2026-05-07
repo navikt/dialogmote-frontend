335b31b51c73de148da09914fea4e26fd4444c9f
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-05-07 13:24:49 +0200

    Disable retry on all HttpError

--- src/common/api/queryRetry.ts
+++ src/common/api/queryRetry.ts
@@ -4,13 +4,7 @@ export const shouldRetryQuery = (
   failureCount: number,
   error: unknown,
 ): boolean => {
-  if (
-    error instanceof HttpError &&
-    error.code >= 400 &&
-    error.code < 500 &&
-    error.code !== 408 &&
-    error.code !== 429
-  ) {
+  if (error instanceof HttpError) {
     return false;
   }
 
