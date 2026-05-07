b04cdb299e237bc412eb6ccdc7248f6417364540
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-05-07 13:24:49 +0200

    Disable retry on all HttpError

--- src/common/api/queryRetry.test.ts
+++ src/common/api/queryRetry.test.ts
@@ -3,31 +3,15 @@ import { HttpError } from "@/common/utils/errors/HttpError";
 import { shouldRetryQuery } from "./queryRetry";
 
 describe("shouldRetryQuery", () => {
-  describe("when the error is a 4xx HttpError that is not retriable", () => {
-    it.each([400, 403, 404, 422])(
-      "should return false for status %s when failureCount is 0",
+  describe("when the error is an HttpError", () => {
+    it.each([400, 404, 408, 429, 500, 502, 503])(
+      "should return false for status %s when failureCount is less than 3",
       (statusCode) => {
-        expect(shouldRetryQuery(0, new HttpError(statusCode))).toBe(false);
+        expect(shouldRetryQuery(2, new HttpError(statusCode))).toBe(false);
       },
     );
 
-    it.each([400, 403, 404, 422])(
-      "should return false for status %s when failureCount is 3",
-      (statusCode) => {
-        expect(shouldRetryQuery(3, new HttpError(statusCode))).toBe(false);
-      },
-    );
-  });
-
-  describe("when the error is a retriable HttpError", () => {
-    it.each([408, 429, 500, 502, 503])(
-      "should return true for status %s when failureCount is less than 3",
-      (statusCode) => {
-        expect(shouldRetryQuery(2, new HttpError(statusCode))).toBe(true);
-      },
-    );
-
-    it.each([408, 429, 500, 502, 503])(
+    it.each([400, 404, 408, 429, 500, 502, 503])(
       "should return false for status %s when failureCount is 3 or more",
       (statusCode) => {
         expect(shouldRetryQuery(3, new HttpError(statusCode))).toBe(false);
